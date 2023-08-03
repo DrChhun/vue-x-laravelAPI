f5bdfe09583b34e112d7ea00d36c99bc85cefbd2
DrChhun <93082266+DrChhun@users.noreply.github.com>
2023-08-04 00:45:03 +0700

    ypdate url

--- src/components/History/HistoryMenu.vue
+++ src/components/History/HistoryMenu.vue
@@ -35,7 +35,7 @@ export default {
 
         const getHistory = async () => {
             const headers = { Authorization: `Bearer j5PxFYtf1g7AH4HR3haVUG5iZdT0oE0uWzE3VJgw` }
-            await axios.get('http://localhost:8000/api/v1/history', { headers })
+            await axios.get('http://128.199.202.99/api/v1/history', { headers })
                 .then(res => historyData.value = res.data.data)
                 .then(err => console.log(err))
         }
